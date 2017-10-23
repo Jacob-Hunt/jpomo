d6c2f4690e90df0f62e25af27e0daf7e69ef10f3
Jacob Hunt <jacobhuntemail@gmail.com>
2017-10-23 03:25:02 -0400

    Implimented dial functions

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,8 +1,36 @@
 // Declare controller
 app.controller('controller', [
-    "$scope",
-    function($scope){
-        //TODO
-        return;        
-    }
+  "$scope",
+  function($scope){
+
+    // String to display in center of timer ring
+    $scope.timerString = "JPomo!";
+
+    // Values of timer settings
+    $scope.intervalVal = 0;
+    $scope.breakVal = 0;
+
+
+    // Functions for adjusting timer settings
+    $scope.incInterval = function(sign){        
+      if(sign > 0){
+        $scope.intervalVal++;
+      } else if (sign < 0) {
+        $scope.intervalVal--;
+      } else {
+        return;
+      }
+    };
+
+    $scope.incBreak = function(sign){
+      if(sign > 0){
+        $scope.breakVal++;
+      } else if (sign < 0) {
+        $scope.breakVal--;
+      } else {
+        return;
+      }
+    };
+
+  }
 ]);
