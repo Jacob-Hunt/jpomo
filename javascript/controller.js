30889282d9bc5332666d969beb7181dd7f807f40
Jacob Hunt <jacobhuntemail@gmail.com>
2017-10-23 03:53:40 -0400

    Sorted into objects; fixed bug

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,36 +1,47 @@
 // Declare controller
 app.controller('controller', [
   "$scope",
+
   function($scope){
 
-    // String to display in center of timer ring
-    $scope.timerString = "JPomo!";
 
-    // Values of timer settings
-    $scope.intervalVal = 0;
-    $scope.breakVal = 0;
-
-
-    // Functions for adjusting timer settings
-    $scope.incInterval = function(sign){        
-      if(sign > 0){
-        $scope.intervalVal++;
-      } else if (sign < 0) {
-        $scope.intervalVal--;
-      } else {
-        return;
-      }
+    $scope.animation = {
+      // String to display in center of timer ring
+      timerString: "JPomo!",
     };
 
-    $scope.incBreak = function(sign){
-      if(sign > 0){
-        $scope.breakVal++;
-      } else if (sign < 0) {
-        $scope.breakVal--;
-      } else {
-        return;
-      }
-    };
 
-  }
+    // Timer settings
+    $scope.settings = {
+
+      // Values of timer settings
+      intervalVal: 25,
+      breakVal: 5,
+
+      methods: {
+        // Adjust interval length setting
+        incInterval: function(sign){        
+          if(sign > 0){
+            $scope.settings.intervalVal++;
+          } else if (sign < 0 && $scope.settings.intervalVal > 0) {
+            $scope.settings.intervalVal--;
+          } else {
+            return;
+          }
+        },
+
+        // Adjust break length setting
+        incBreak: function(sign){
+          if(sign > 0){
+            $scope.settings.breakVal++;
+          } else if (sign < 0 && $scope.settings.breakVal > 0) {
+            $scope.settings.breakVal--;
+          } else {
+            return;
+          }
+        },
+      },
+
+    };
+  },
 ]);
